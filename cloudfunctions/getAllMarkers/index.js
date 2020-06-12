c5ad98536b0325cd6898a03e137f3cc8af8a5df4
xlzy <1017866168@qq.com>
2020-06-12 23:07:05 +0800

    add: 增加云函数

---
+++ cloudfunctions/getAllMarkers/index.js
@@ -0,0 +1,22 @@
+// 云函数模板
+// 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
+
+const cloud = require('wx-server-sdk')
+
+// 初始化 cloud
+cloud.init({
+  // API 调用都保持和云函数当前所在环境一致
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+/**
+ * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
+ *
+ * event 参数包含小程序端调用传入的 data
+ *
+ */
+exports.main = async (event, context) => {
+  const db = cloud.database()
+  return await db.collection('mapMarkers').limit(200).get()
+}
+
