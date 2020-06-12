beea7572b4093e820e30d46998075538cb40d43e
xlzy <1017866168@qq.com>
2020-06-12 23:07:33 +0800

    update: 更新腾讯地图key

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 // 腾讯地图KEY，自行申请：https://lbs.qq.com
-export const MAP_KEY = 'GEWBZ-3GZCP-34CDS-LAIP7-IATVO-LWFLQ' // 最好自己去申请一个，随时可能会被删除
+export const MAP_KEY = 'TMHBZ-44NKU-RIEVH-26FJG-6XSI6-TYFPG' // 最好自己去申请一个，随时可能会被删除
