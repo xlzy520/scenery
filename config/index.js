2b58c3e7f1fb4c6c1545ebfce16f98992ccb35ad
xlzy <1017866168@qq.com>
2020-06-19 10:47:16 +0800

    update: 增肌默认经纬度

--- config/index.js
+++ config/index.js
@@ -1,2 +1,7 @@
 // 腾讯地图KEY，自行申请：https://lbs.qq.com
 export const MAP_KEY = 'TMHBZ-44NKU-RIEVH-26FJG-6XSI6-TYFPG' // 最好自己去申请一个，随时可能会被删除
+
+export const Default_Location = {
+  latitude: '119.46242',
+  longitude: '35.42545'
+}
