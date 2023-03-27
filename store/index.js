ec732728e3086860c2dfa4a9c9fdec95bb564742
xlzy <1017866168@qq.com>
2023-03-27 16:51:39 +0800

    移除logger

--- store/index.js
+++ store/index.js
@@ -6,9 +6,6 @@ import actions from './actions'
 import mutations from './mutations'
 import state from './state'
 
-import Logger from 'vuex/dist/logger'
-const debug = process.env.NODE_ENV !== 'production'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -16,6 +13,4 @@ export default new Vuex.Store({
   actions,
   mutations,
   state,
-  strict: debug,
-  plugins: debug ? [Logger()] : []
 })
